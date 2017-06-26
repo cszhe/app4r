cf435096e30c12d19b02dac11b06229ca6da2d65
Jason He <jason.he@auckland.ac.nz>
2017-06-27 11:24:48 +1200

    Accelerometer added

--- android/Sensors/app/src/main/java/nz/ac/auckland/zhe203/sensors/MainActivity.java
+++ android/Sensors/app/src/main/java/nz/ac/auckland/zhe203/sensors/MainActivity.java
@@ -1,13 +1,51 @@
 package nz.ac.auckland.zhe203.sensors;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.hardware.*;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener{
+
+
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tv = (TextView)this.findViewById(R.id.tvDisp);
+
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor accelerometer = null;
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            // success! we have an accelerometer
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        } else {
+            tv.setText("No Accelerometer Detected");
+        }
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+        String disp = String.format("x=%f, y=%f, z=%f", x, y, z);
+
+        this.tv.setText(disp);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
