a1f97bcf10698943abf901555680f5e13c8bf1c9
Jason He <jason.he@auckland.ac.nz>
2017-06-28 20:07:50 +1200

    accelerometer in iPhone

--- ios/sensors/sensors/ViewController.swift
+++ ios/sensors/sensors/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var lblAcc: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let manager = CMMotionManager()
+        if manager.isAccelerometerAvailable {
+            manager.accelerometerUpdateInterval = 0.1
+            manager.startAccelerometerUpdates(to: OperationQueue.main, withHandler: { (data, err) in
+                if let acc = data?.acceleration {
+                    self.lblAcc.text = "X = \(acc.x) Y = \(acc.y) Z = \(acc.z)"
+                } else {
+                    self.lblAcc.text = "No data"
+                }
+            })
+        }
     }
 
     override func didReceiveMemoryWarning() {
